43094973eea9319c3a1b724d30b0ef44cc7ee393
Rahesh2626 <rahesh.kulkarni2626@gmail.com>
2025-05-10 17:40:02 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import re, random 
+destinations = {
+    "beaches": ["Lakshyadweep", "Mumbai", "Goa"],
+    "mountains" : ["Himalayas","Rocky MAountains", "Sahyadri"],
+    "cities": ["Delhi","Chennai","Patna"]
+}
+jokes = [
+    "What don't programmer like nature ? Too many bugs",
+    "Why did the computer don not go to Doctor ? because it had a virus "
+    "Why do traveller always fell warm ? because of all their hot spots"
+]
